a7ea4366453a8ce58cac531caa1e8a10e54cf7e9
Napa <napa_tom@hotmail.com>
2016-11-08 17:30:16 +0700

    test

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace SuperCMS_API
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
+            //TEST
             app.UseMvc();
         }
     }
